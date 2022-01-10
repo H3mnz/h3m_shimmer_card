af7b97fad3c007e6f3031258cd48b2ad3d93631d
H3mnz <huckerz30@gmail.com>
2022-01-10 23:15:26 +0330

    Fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:h3m_shimmer_card/shimmer_card.dart';
+import 'package:h3m_shimmer_card/h3m_shimmer_card.dart';
 
 void main() {
   runApp(const MyApp());
